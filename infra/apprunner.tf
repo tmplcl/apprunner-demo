c58504bf82038859bbd190824d10266be00a6f32
timurphilipcol <qjLJj#d,E&XaC4`.h/_rVhlX^RL_C`YI>
2023-05-03 14:00:23 +0200

    Fix apprunner repo source config

--- infra/apprunner.tf
+++ infra/apprunner.tf
@@ -1,14 +1,25 @@
+resource "aws_apprunner_connection" "this" {
+  connection_name = "tmplcl-1"
+  provider_type   = "GITHUB"
+
+}
+
 resource "aws_apprunner_service" "this" {
   service_name = "apprunner-demo"
 
   source_configuration {
+    authentication_configuration {
+      connection_arn = aws_apprunner_connection.this.arn
+    }
     code_repository {
       repository_url = "https://github.com/tmplcl/apprunner-demo"
       source_code_version {
         type  = "BRANCH"
         value = "main"
       }
-      configuration_source = "REPOSITORY"
+      code_configuration {
+        configuration_source = "REPOSITORY"
+      }
     }
   }
 
